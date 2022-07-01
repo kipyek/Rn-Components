46aaa8e10e8b43a2ec31cf94749be6b931aaaa95
kipyek <deniskipyegon88@gmail.com>
2022-07-01 09:03:45 +0300

    BottomTabs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import BottomSheet from './Screens/BottomSheet'
+import BottomTabs from './Screens/BottomTabs'
 
 const App = () => {
   return (
 
-    <BottomSheet />
+    <BottomTabs />
 
   )
 }

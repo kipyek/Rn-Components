622fbff26a2daa5487e76e4368cd8ccb25e05b0c
kipyek <deniskipyegon88@gmail.com>
2022-07-05 08:42:23 +0300

    OnboardingScreen

--- App.tsx
+++ App.tsx
@@ -2,13 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import BottomSheet from './Screens/BottomSheet'
 import BottomTabs from './Screens/BottomTabs'
+import OnboardingScreen from './Screens/OnboardingScreen'
 
 const App = () => {
   return (
 
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, backgroundColor: 'grey' }}>
-      <Text>Hello!!</Text>
-    </View>
+    <OnboardingScreen />
 
   )
 }
